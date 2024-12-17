23c745b6bbee779bbddaf711cb209df953ace03e
Rahmi Kutay Özcan <rahmikutayozcan@gmail.com>
2024-12-18 01:21:40 +0300

    If Else practice added

---
+++ Bootcamp/Week1/IfElse/IfElse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfElse{
+    class Program{
+
+        static void Main(string[] args){
+
+            Console.Write("Lutfen bir sayi giriniz: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            if(number > 10){
+                Console.WriteLine("Girilen sayi 10'dan buyuktur.");
+            }
+            else if(number < 10){
+                Console.WriteLine("Girilen sayi 10'dan kucuktur.");
+            }
+            else{
+                Console.WriteLine("Girilen sayi 10'a esittir.");
+            }
+
+            if(number % 2 == 0){
+                Console.WriteLine("Girilen sayi cifttir.");
+            }
+            else{
+                Console.WriteLine("Girilen sayi tektir.");
+            }
+        }
+    }
+}
