3cf92ca1a063b2808ec6f6df1c5acf00f50f6e6d
Rahmi Kutay Özcan <rahmikutayozcan@gmail.com>
2024-12-17 23:16:09 +0300

    Add files via upload

---
+++ Bootcamp/Week1/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HelloWorld{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rahmi Kutay Ozcan");
+            Console.WriteLine("Patika'nin katılımcılarına sagladigi genis kapsamlı egitim programi ile universitede aldigim yazilim muhendisligi bilgilerimi harmanlayip sektore guclu bir giris yapmak.");
+        }
+    }
+
+
+}
+
+
+
