b32143df1b05f22d78cf9686ade8d125b6c83da1
Rahmi Kutay Özcan <rahmikutayozcan@gmail.com>
2025-01-10 15:05:31 +0300

    Week5 and Array added

---
+++ Bootcamp/Week5/Array/Program.cs
@@ -0,0 +1,35 @@
+﻿Random rnd = new Random();
+// 1. Create an array with 10 elements
+int[] array = new int[10];
+
+// 2. Fill the array with for loop
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 100);
+}
+
+// Print the array with foreach loop
+Console.Write("The random array is: ");
+foreach (var i in array)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+
+// 3. Resize and add new number
+Console.Write("Enter new number for array:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Array.Resize(ref array, 11);
+array[array.Length - 1] = number;
+
+// 4. Sort the array in descending order
+System.Array.Sort(array);
+System.Array.Reverse(array);
+
+Console.Write("Sorted array is: ");
+foreach (var i in array)
+{
+    Console.Write(i + " ");
+}
