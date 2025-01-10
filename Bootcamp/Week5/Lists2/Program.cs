477ba70d4abd0bfa70d823b9a7e543166fa0f6dd
Rahmi Kutay Özcan <rahmikutayozcan@gmail.com>
2025-01-10 22:25:03 +0300

    Lists2 added

---
+++ Bootcamp/Week5/Lists2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter 5 different coffee type for cafe menu !");
+
+List<string?> coffees = new List<string?>(); // Creating string list for menu
+
+for (int i = 0; i < 5; i++)  // Getting coffee types from user
+{
+    Console.Write($"Coffee {i+1}: ");
+    coffees.Add(Console.ReadLine());
+}
+Console.Clear();
+
+Console.WriteLine("Coffee Menu: ");   // Displaying menu
+foreach (var coffee in coffees)
+{
+    Console.WriteLine(coffee);
+}
