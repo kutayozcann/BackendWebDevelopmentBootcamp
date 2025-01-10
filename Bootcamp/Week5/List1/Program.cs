f5cdee14b9fcb91875ef1039c26d5810d1c46d28
Rahmi Kutay Özcan <rahmikutayozcan@gmail.com>
2025-01-10 22:04:37 +0300

    List1 added

---
+++ Bootcamp/Week5/List1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Creating and Adding guests to list
+List<string> guests = new List<string>()
+{
+    "Bülent Ersoy",
+    "Ajda Pekkan",
+    "Ebru Gündeş",
+    "Hadise",
+    "Hande Yener",
+    "Tarkan",
+    "Funda Arar",
+    "Demet Akalın"
+};
+
+// Display Guests List
+Console.WriteLine("** Guest List **");
+foreach (var person in guests)
+{
+    Console.Write(guests.IndexOf(person) + 1 + " - ");
+    Console.WriteLine(person);
+}
